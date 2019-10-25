a1329a0dee7f8a851a2d3a56b37689b46aa81b0e
Grant Wilson <gcw15@pitt.edu>
2019-10-25 15:05:57 -0400

    In this step, I coded for the tiger data table.

--- GitPrac.R
+++ GitPrac.R
@@ -1,3 +1,9 @@
 #This is a practice script for git on final project
 
 tigerData <- read.csv(url("http://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter02/chap02e2aDeathsFromTigers.csv"))
+
+tigerTable <- sort(table(tigerData$activity), decreasing = TRUE)
+
+tigerTable
+
+
